7e31a52c464d5a0236532895ad2e910442d61e2e
antonlau-1 <antony.hy.lau@gmail.com>
2024-09-23 13:56:49 -0400

    dockerised

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,17 @@ require('dotenv').config(); // Load environment variables
 
 const connectDB = async () => {
   try {
-    // Construct MongoDB URI using environment variables
-    const uri = `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASS}@${process.env.MONGOHOST}:${process.env.MONGOPORT}/${process.env.MONGONAME}`;
+    // Log environment variables for debugging
+    console.log('MONGOUSER:', process.env.MONGOUSER);
+    console.log('MONGOPASS:', process.env.MONGOPASS);
+    console.log('MONGOHOST:', process.env.MONGOHOST);
+    console.log('MONGOPORT:', process.env.MONGOPORT);
+    console.log('MONGONAME:', process.env.MONGONAME);
+
+    // Encode the database name to handle spaces or special characters
+    const encodedDBName = encodeURIComponent(process.env.MONGONAME);
+    const uri = `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASS}@${process.env.MONGOHOST}:${process.env.MONGOPORT}`;
+    console.log('MongoDB URI:', uri); // Log URI for debugging
     
     await mongoose.connect(uri, {
       useNewUrlParser: true,
